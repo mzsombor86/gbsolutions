7d0ccfb69b3b1bc2c2e43ca2024af2364ed83767
mzsombor86 <mzsombor@gmail.com>
2021-06-24 16:24:59 +0200

    First commit of framework.

---
+++ JobmonitorAPI/src/main/java/hu/mzsombor/jobmonitor/JobmonitorApiApplication.java
@@ -0,0 +1,13 @@
+package hu.mzsombor.jobmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmonitorApiApplication.class, args);
+	}
+
+}
---
+++ JobmonitorAPI/src/test/java/hu/mzsombor/jobmonitor/JobmonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mzsombor.jobmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
